db0bb97863b8ef4b9c1531e5acd261102c8b115a
tburke <tylerburke@live.com.au>
2021-09-07 19:59:03 +1000

    ci(build): remove .build folder

--- .build/src/domain/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.META_SCOPES = exports.META_REQUEST_MAPPING = exports.HandlerExecuteMethod = exports.TKN_CONTEXT = exports.TKN_LOGGER = void 0;
-exports.TKN_LOGGER = "TOKEN_LOGGER";
-exports.TKN_CONTEXT = "TOKEN_CONTEXT";
-exports.HandlerExecuteMethod = "execute";
-exports.META_REQUEST_MAPPING = "meta:request-mapping";
-exports.META_SCOPES = "meta:oauth2-scopes";
--- .build/src/domain/enums/injectableType.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InjectableType = void 0;
-var InjectableType;
-(function (InjectableType) {
-    InjectableType["LOGGER"] = "LOGGER";
-})(InjectableType = exports.InjectableType || (exports.InjectableType = {}));
